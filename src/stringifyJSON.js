d15e34eff1c90db95df427ebe9c6354a47c35b88
Alex Figueroa <alexfig221@gmail.com>
2015-08-12 12:48:08 -0700

    Completed getElementsByClassName and stringifyJSON methods

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,29 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if(obj === null) return "null";
+
+  else if(typeof obj === 'string') 
+    return '"' + obj + '"'; 
+
+  else if(typeof obj === 'boolean' || typeof obj === 'number') 
+    return obj.toString();
+
+  else if(Array.isArray(obj)){
+    if(obj.length ===0) return '[]';
+    var result = _.reduce(obj, function(str, item) {
+        return str + stringifyJSON(item) + ',';
+    }, '[');
+    return result.slice(0, - 1) + ']';
+
+  } else if(typeof obj === 'object'){
+      var result = '{';
+      _.each(obj, function(value, key){
+        if(typeof value === 'undefined' || typeof value === 'function') return;
+        result = result + '"' + key +'":' + stringifyJSON(value) + ',';
+      });
+      if(result.length === 1) return "{}";
+      return result.slice(0, - 1) + '}';
+  }
+  return "null";
 };
